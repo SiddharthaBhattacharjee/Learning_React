f7af5fbe9c93b6a06590cd6600b29dcc3e9b1e02
Siddhartha Bhattacharjee <siddharthabhatt2345@gmail.com>
2022-09-25 22:51:38 +0530

    React4.2

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+let name = "User";
+let question = "Wnat is your name ?"
 function App() {
+  const handleClick = event => {
+    name = document.getElementById("yname").value;
+    console.log(name);
+    document.getElementById("greet").classList.remove("hidea");
+    document.getElementById("greet").innerHTML = "Hello "+name+", Thank You for checkin out my website, hope you have an amazing day :)";
+  };
   return (
     <div className="empty">
-      Hello World
+      <p>Hello World </p>
+      <div id="container">
+        <p>{question}</p>
+        <input type="text" id="yname"/>
+        <button id="Enter" onClick={handleClick}>Confirm</button>
+        <p id="greet" className="hidea">Hello {name} , Thank You for checkin out my website, hope you have an amazing day :)</p>
+      </div>
     </div>
   );
 }
