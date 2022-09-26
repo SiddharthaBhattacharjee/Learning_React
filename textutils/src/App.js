058a1cdb1ed37205e92757b603f4d0c53979cac0
Siddhartha Bhattacharjee <siddharthabhatt2345@gmail.com>
2022-09-26 16:44:28 +0530

    React5.2

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,33 +3,19 @@ import './App.css';
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-dark" >
-    <div className="container-fluid" >
-      <a className="navbar-brand" href="/" style={{color:"white"}}>Navbar</a>
+    <nav className="navbar navbar-expand-lg bg-light" >
+    <div className="container-fluid">
+      <a className="navbar-brand" href="/" >Navbar</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item" >
-            <a className="nav-link active" aria-current="page" href="/" style={{color:"white"}} >Home</a>
+            <a className="nav-link active" aria-current="page" href="/"  >Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/" style={{color:"white"}}>Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color:"white"}}>
-              Dropdown
-            </a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="/">Action</a></li>
-              <li><a className="dropdown-item" href="/">Another action</a></li>
-              <li><hr className="dropdown-divider" /></li>
-              <li><a className="dropdown-item" href="/">Something else here</a></li>
-            </ul>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" style={{color:"white"}}>Disabled</a>
+            <a className="nav-link" href="/" >About</a>
           </li>
         </ul>
         <form className="d-flex" role="search">
