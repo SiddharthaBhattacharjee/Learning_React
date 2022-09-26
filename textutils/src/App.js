0ec0ca66e3fb919082aaf2c3dd32f3700267d2c1
Siddhartha Bhattacharjee <siddharthabhatt2345@gmail.com>
2022-09-26 16:22:25 +0530

    React5.1

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,27 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <>
-    <p>I can only return 1 html element, so I wrapped both the elements in a JSX fragment.</p>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am Learning React, This is my first step :)
-        </a>
-      </header>
+    <nav className="navbar navbar-expand-lg bg-dark" >
+    <div className="container-fluid" >
+      <a className="navbar-brand" href="/" style={{color:"white"}}>Navbar</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item" >
+            <a className="nav-link active" aria-current="page" href="/" style={{color:"white"}} >Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/" style={{color:"white"}}>Link</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color:"white"}}>
+              Dropdown
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="/">Action</a></li>
+              <li><a className="dropdown-item" href="/">Another action</a></li>
+              <li><hr className="dropdown-divider" /></li>
+              <li><a className="dropdown-item" href="/">Something else here</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" style={{color:"white"}}>Disabled</a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit" >Search</button>
+        </form>
+      </div>
     </div>
-    </>
+  </nav>
+  </>
     
   );
 }
